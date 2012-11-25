c4c99107d5d62f0efe1aaf027fcd8dee97e334b8
David Desktop Win8 <houliston.david@gmail.com>
2012-11-25 09:54:12 +0000

    initial and flights design

---
+++ LogbookApp/LogbookApp/Model/Flight.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogbookApp.Model
+{
+    public class Flight
+    {
+        public DateTime Date { get; set; }
+        public string AcType { get; set; }
+        public string Reg { get; set; }
+        public string Captain { get; set; }
+        public string Capacity { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public DateTime Depart { get; set; }
+        public DateTime Arrival { get; set; }
+
+    }
+}
---
+++ LogbookApp/LogbookApp/ViewModel/FlightsPageViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LogbookApp.Model;
+using System.Collections.ObjectModel;
+
+namespace LogbookApp.ViewModel
+{
+    public class FlightsPageViewModel
+    {
+        public FlightsPageViewModel()
+        {
+
+            Flights = new ObservableCollection<Flight>();
+            for (int i = 0; i < 100; i++)
+            {
+                Flights.Add(new Flight
+                {
+                    Date = new DateTime(2012, 2, 2),
+                    AcType = "C-152",
+                    Reg = "G-ABCD",
+                    Captain = "Self",
+                    Capacity = "P1",
+                    From = "Fairoaks",
+                    To = "Goodwood",
+                    Depart = new DateTime(2012, 2, 2, 10, 15, 0),
+                    Arrival = new DateTime(2012, 2, 2, 11, 15, 0)
+                });
+            }
+            
+            
+        }
+
+        public ObservableCollection<Flight> Flights { get; set; }
+    }
+}
