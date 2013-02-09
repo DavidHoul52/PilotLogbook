5570314ec7c23ded1b7d09719e5e0c1c00ad6aa8
David Desktop Win8 <houliston.david@gmail.com>
2013-02-09 18:35:09 +0000

    Time > 24 hrs

--- LogbookApp/LogbookApp/Common/TimeSpanConverter.cs
+++ LogbookApp/LogbookApp/Common/TimeSpanConverter.cs
@@ -12,7 +12,12 @@ namespace LogbookApp.Common
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             if (value is TimeSpan)
-                return ((TimeSpan)value).ToString(@"hh\:mm");
+            {
+                TimeSpan timeSpan = (TimeSpan)value;
+                if (timeSpan.Days==0)
+                  return ((TimeSpan)value).ToString(@"hh\:mm");
+                return string.Format("{0}:{1}", timeSpan.TotalHours.ToString("00"), timeSpan.Minutes.ToString("00"));
+            }
             return null;
         }
 
